dc366c1f2587c5fc29b06de3728fd92a67626313
kubano <jakubno3ak@gmail.com>
2023-09-02 17:17:31 +0200

    Setup VPC for the-cool-ai-system

--- infra/variables.tf
+++ infra/variables.tf
@@ -20,3 +20,21 @@ variable "tags" {
     type        = map(string)
     default     = {}
 }
+
+variable "public_subnet_cidrs" {
+    type        = list(string)
+    description = "Public Subnets CIDRs"
+    default     = ["10.0.1.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+    type        = list(string)
+    description = "Private Subnets CIDRs"
+    default     = ["10.0.4.0/24"]
+}
+
+variable "availability_zones" {
+    type = list(string)
+    description = "Availability Zones"
+    default = ["eu-west-1a"]
+}
---
+++ infra/vpc.tf
@@ -0,0 +1,94 @@
+resource "aws_vpc" "the-cool-ai-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  instance_tenancy = "default"
+  tags = {
+    Name = "TheCoolAIVPC"
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  count  = length(var.public_subnet_cidrs)
+  vpc_id = aws_vpc.the-cool-ai-vpc.id
+  cidr_block = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+  tags = {
+    Name = "PublicSubnet ${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count  = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.the-cool-ai-vpc.id
+  cidr_block = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "PrivateSubnet ${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "the_cool_ai_igw" {
+  vpc_id = aws_vpc.the-cool-ai-vpc.id
+
+  tags = {
+    Name = "TheCoolAIIGW"
+  }
+}
+
+resource "aws_route_table" "rt_for_public" {
+  vpc_id = aws_vpc.the-cool-ai-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.the_cool_ai_igw.id
+  }
+
+  tags = {
+    Name = "TheCoolAIPublicRT"
+  }
+}
+
+resource "aws_route_table_association" "public_subnets_rt_association" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.rt_for_public.id
+}
+
+resource "aws_security_group" "for_public_resources" {
+  name = "the_cool_ai_sg_allowing_for_public_on_3000"
+  vpc_id = aws_vpc.the-cool-ai-vpc.id
+  description = "This is SG for model exposed publicly"
+
+  ingress {
+    from_port = 3000
+    to_port   = 3000
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    self = true
+  }
+
+   egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "TheCoolAIModelSG"
+  }
+}
